9838f9333adaa37c8fa8217b10cc0653195f0784
George <george_zakko@hotmail.com>
2026-03-23 18:45:35 -0400

    Made it so reviews go into the raw json file. Also fixed the id of the app to not include the language.

--- src/01_collect_or_import.py
+++ src/01_collect_or_import.py
@@ -7,7 +7,7 @@ from google_play_scraper import Sort, reviews_all
 
 
 calm_reviews = reviews_all(
-    'com.calm.android&hl=en_CA',
+    'com.calm.android',
     sleep_milliseconds=0, # defaults to 0
     lang='en', # defaults to 'en'
     country='us', # defaults to 'us'
@@ -17,7 +17,5 @@ df_calm = pd.DataFrame(np.array(calm_reviews),columns=['review'])
 
 
 df_calm = df_calm.join(pd.DataFrame(df_calm.pop('review').tolist()))
+df_calm.to_json("data/reviews_raw.jsonl", orient="records", lines=True)
 
-
-
-df_calm.head()
