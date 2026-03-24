cca8c79a14d7ffaccc910e0df89a8f07351dc83a
George <george_zakko@hotmail.com>
2026-03-24 18:50:30 -0400

    Just started cleaning the data by dropping dupes, empty entries, and short reviews.

--- src/02_clean.py
+++ src/02_clean.py
@@ -1,1 +1,12 @@
 """cleans raw data & make clean dataset"""
+import pandas as pd
+
+df = pd.read_json("data/reviews_raw.jsonl", lines=True)
+df.drop_duplicates(inplace=True)
+
+df = df[df['content'].notna() & (df['content'] != '')]
+
+# The valid review length must be greater than 20 because arbitrarily set.  
+df = df[df['content'].str.len() > 20]
+
+df.to_json("data/reviews_clean.jsonl", orient="records", lines=True)
