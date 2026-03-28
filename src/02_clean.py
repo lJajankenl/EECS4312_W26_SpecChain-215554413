30eecdf2759f4efe081d807d60c641a25c831af8
George <george_zakko@hotmail.com>
2026-03-28 16:46:17 -0400

    Added cleaning logic so punctation, special characters, and emojis are all removed. Also added logic to convert all uppercase characters to lowercase. Still working on numbers to text.

--- src/02_clean.py
+++ src/02_clean.py
@@ -1,5 +1,6 @@
 """cleans raw data & make clean dataset"""
 import pandas as pd
+import num2words
 
 df = pd.read_json("data/reviews_raw.jsonl", lines=True)
 df.drop_duplicates(inplace=True)
@@ -8,5 +9,20 @@ df = df[df['content'].notna() & (df['content'] != '')]
 
 # The valid review length must be greater than 20 because arbitrarily set.  
 df = df[df['content'].str.len() > 20]
+
+# Remove punctuation, special characters, and emojis.  
+df['content'] = df['content'].str.replace(r"[^\w\s]", "", regex=True)
+
+# Convert numbers to text
+#df['content'] = df['content'].str.replace(r"\d+", num2words(), regex=True)
+
+# Remove extra whitespace
+
+# Convert all words to lowercase
+df['content'] = df['content'].str.lower()
+
+# Remove stop words
+
+# Lemmatize the reviews
 
 df.to_json("data/reviews_clean.jsonl", orient="records", lines=True)
